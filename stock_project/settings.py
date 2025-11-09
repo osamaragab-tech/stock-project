bf5482a7ef1f2d6cb4b826554b2788dcd1e4c253
osamaragab2022 <osragab2022@gmail.com>
2025-11-10 00:42:18 +0200

    update setting.py

--- stock_project/settings.py
+++ stock_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@_ww-fayi06&byz=k4tmh!sv4@j&3o@32qgf3%tdn4tjnz#%bp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['osamaomar.pythonanywhere.com', 'osamaomar.pythonanywhere.com/stock_project', 'localhost', '127.0.0.1', '192.168.1.5']
 
